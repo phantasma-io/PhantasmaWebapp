0044688b966f1ff1d0c7519b8bfde7cf0e1a09e7
JPPinho <joaopedrorochapinho@gmail.com>
2018-08-21 20:33:53 +0100

    Initialization - Yii2 Advanced Template

---
+++ frontend/config/dataConfig.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'text' => [
+        'data' => 'string'
+    ],
+    'DictionaryVariable' => [
+        'variable' => 'string',
+        'value' => 'string/null'
+    ],
+    'SingleVariable' => [
+        'variable' => 'string',
+        'value' => 'string/null'
+    ],
+    'Image' => [
+        'minSize'=> 'float/null',
+        'maxSize'=> 'float/null'
+    ],
+
+
+];
